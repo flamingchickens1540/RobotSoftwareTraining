13e678e11f1c1442b1240186fcd85de5eb3e27e6
belowAverageCoder5188 <146988904+belowAverageCoder5188@users.noreply.github.com>
2023-11-02 16:38:27 -0700

    Feat: Inital Commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,12 +4,22 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation.
  */
+import edu.wpi.first.wpilibj.XboxController;
 public class Robot extends TimedRobot {
+  XboxController controller = new XboxController(0);
+  TalonSRX frontLeft = new TalonSRX(1);
+  TalonSRX backLeft = new TalonSRX(2);
+  TalonSRX frontRight = new TalonSRX(4);
+  TalonSRX backRight = new TalonSRX(3);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -17,6 +27,18 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
+    // frontLeft.setNeutralMode(NeutralMode.Brake);
+    // frontRight.setNeutralMode(NeutralMode.Brake);
+    // backLeft.setNeutralMode(NeutralMode.Brake);
+    // backRight.setNeutralMode(NeutralMode.Brake);
+
+    backLeft.setNeutralMode(NeutralMode.Coast);
+
+    // frontLeft.setInverted(true);
+    // backLeft.setInverted(true);
+
+    backLeft.follow(frontLeft);
+    backRight.follow(frontRight); 
   }
 
   /**
@@ -32,5 +54,16 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    int deadZoneLeft = 1;
+    int deadZoneRight = 1;
+    if (Math.abs(controller.getLeftY()) <= 0.1) {
+      deadZoneLeft = 0;
+    }
+    if (Math.abs(controller.getRightY()) <= 0.1) {
+      deadZoneRight = 0;
+    }
+    frontLeft.set(ControlMode.PercentOutput, controller.getLeftY() * 0.1 * deadZoneLeft);
+    frontRight.set(ControlMode.PercentOutput, controller.getRightY() * 0.1 * deadZoneRight);
+  }
 }
